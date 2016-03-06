c78325a3f1459392cb6f9e761eda123ac30c2e07
Rafael Reyes <rafaelrreyes@yahoo.com>
2016-03-05 22:48:41 -0800

    Adding client controller and comments to HexagonTile class

--- src/catan/HexagonTile.java
+++ src/catan/HexagonTile.java
@@ -8,19 +8,23 @@ public class HexagonTile {
 	private int rollNumber;
 	private String resourceType;
 	
+	//main constructor for a hexagon tile, usually created at the beginning of game
 	public HexagonTile() {
 		this.resourceType = null;
 		this.rollNumber = 0;
 	}
 	
+	//sets the resource type located on this tile
 	public void setResourceType(String resource) {
 		this.resourceType = resource;
 	}
 	
+	//sets the roll number located on this tile
 	public void setRollNumber(int num) {
 		this.rollNumber = num;
 	}
 	
+	//checks if the current tile contains the number rolled by current player
 	public boolean contains(int diceRoll) {
 		if(this.getRollNumber() == diceRoll) {
 			return true;
@@ -30,10 +34,12 @@ public class HexagonTile {
 		
 	}
 	
+	//gets the resource type located on this tile
 	public String getResourceType() {
 		return this.resourceType;
 	}
 	
+	//gets the roll number located on this tile
 	public int getRollNumber() {
 		return this.rollNumber;
 	}
